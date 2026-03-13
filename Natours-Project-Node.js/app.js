b3b2a33aed6e8b8b35d3f187f5afa893562fb014
SanaTameem <sanatameem38@gmail.com>
2026-03-13 22:24:01 +0430

    Update  Natours-Project-Node.js/app.js file

--- Natours-Project-Node.js/app.js
+++ Natours-Project-Node.js/app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const fs = require('fs');
+const app = express();
 
 //when doing a post request, we need this middleware to be able to read the body of the request and parse it into a JavaScript object
-
-const app = express();
 app.use(express.json());
 
-//Get all Tour:
+//get,post,patch,delete are http methods, '/api/v1/tours' is called route, (req, res) => {} is the route handler function that will be executed when a request is made to this route.
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
-app.get('/api/v1/tours', (req, res) => {
+//Route Handlers:
+
+const getAllTours = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', result: tours.length, data: { tours } });
-});
+};
 
-//Get a specific Tour:
-//:id is a placeholder for the actual id of the tour that we want to get. We can access this id using req.params.id
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   //req.params.id is a string, we need to convert it to a number usin * 1
 
   const tour = tours.find((el) => el.id === req.params.id * 1);
@@ -27,10 +27,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
   }
   res.status(200).json({ status: 'success', data: { tour } });
-});
+};
 
-//Post a new Tour:
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   newId = tours[tours.length - 1].id + 1;
   //Object.assign() is a method that combines two or more objects into one.
   const newTour = Object.assign({ id: newId }, req.body);
@@ -43,31 +42,36 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     },
   );
-});
+};
 
-//PUT updates the whole resource but patch only updates the fields that we want to update.
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   console.log(req.params.id);
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
   }
-
   res
     .status(200)
     .json({ status: 'success', data: { tour: 'Updated tour here...' } });
-});
+};
 
-//Delete a Tour:
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
   }
 
   res.status(204).json({ status: 'success', data: null });
-});
+};
+
+//Routes
+app.get('/api/v1/tours', getAllTours);
+//:id is a placeholder for the actual id of the tour that we want to get. We can access this id using req.params.id
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+//PUT updates the whole resource but patch only updates the fields that we want to update.
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`app listens on port ${port}`);
 });
